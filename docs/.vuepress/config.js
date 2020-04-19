bd38f9d2fda76db1d3df1174b95e4599b4a05f14
toshihirokato <nanamin.104@icloud.com>
2020-04-19 17:55:10 +0900

    ルーティングの設定

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,23 +1,40 @@
 module.exports = {
-  theme: 'onen',
+  theme: "onen",
   themeConfig: {
-    menu: [
+    navbar: [
       {
-        text: 'postA',
-        link: '/post/postA',
-        children: [
-          { text: 'postA1', link: '/postA-1' },
-          { text: 'postA2', link: '/postA-2' },
-        ]
+        type: "home",
+        title: "Home",
+        href: "/",
       },
       {
-        text: 'postB',
-        link: '/post/postB',
+        type: "user",
+        title: "About",
+        href: "/about",
+      },
+      {
+        type: "github",
+        title: "Github",
+        href: "https://github.com/torish14",
+      },
+    ],
+    menu: [
+      {
+        text: "postA",
+        link: "/post/postA",
         children: [
-          { text: 'postB1', link: '/postB-1' },
-          { text: 'postB2', link: '/postB-2' },
-        ]
-      }
-    ]
-  }
-}
+          { text: "postA1", link: "/postA-1" },
+          { text: "postA2", link: "/postA-2" },
+        ],
+      },
+      {
+        text: "postB",
+        link: "/post/postB",
+        children: [
+          { text: "postB1", link: "/postB-1" },
+          { text: "postB2", link: "/postB-2" },
+        ],
+      },
+    ],
+  },
+};
